f565a5723af64d2b53704ec676eff46dceb10a57
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2024-10-11 10:19:54 +0900

    className=bodyの削除

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -7,31 +7,29 @@ const Chat = () => {
   //   setMessage(e.target.value);
   // }
   return (
-    <div className={styles.body}>
-      <div className={styles.chat_container}>
-        <div className={styles.chat_box}>
-          <div className={styles.message}>
-            <p className={styles.user_name}>ユーザ1</p>
-            <p className={styles.message_bubble}>メッセージ内容</p>
-          </div>
+    <div className={styles.chat_container}>
+      <div className={styles.chat_box}>
+        <div className={styles.message}>
+          <p className={styles.user_name}>ユーザ1</p>
+          <p className={styles.message_bubble}>メッセージ内容</p>
+        </div>
 
-          <div className={styles.message}>
-            <p className={styles.user_name}>自分</p>
-            <p className={styles.message_bubble}>メッセージ内容</p>
-          </div>
+        <div className={styles.message}>
+          <p className={styles.user_name}>自分</p>
+          <p className={styles.message_bubble}>メッセージ内容</p>
         </div>
-        <div className={styles.input_box}>
-          <input
-            type="text"
-            value={message}
-            placeholder="メッセージを入力"
-            // onChange={handleChange}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setMessage(e.target.value)
-            }
-          />
-          <button type="submit">送信</button>
-        </div>
+      </div>
+      <div className={styles.input_box}>
+        <input
+          type="text"
+          value={message}
+          placeholder="メッセージを入力"
+          // onChange={handleChange}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setMessage(e.target.value)
+          }
+        />
+        <button type="submit">送信</button>
       </div>
     </div>
   );
