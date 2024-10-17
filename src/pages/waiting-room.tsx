b24519ccc0f64fc2e0da8b2b673fcf37763c6bfd
steve goodjobs <taku.hayakawa@rakus-partners.co.jp>
2024-10-17 12:07:57 +0900

    待機画面のHTML

---
+++ src/pages/waiting-room.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WaitingRoom from '../components/WaitingRoom/waiting-room'; // コンポーネントのパスを修正
+
+const WaitingRoomPage = () => {
+  return (
+    <div>
+      <WaitingRoom />
+    </div>
+  );
+};
+
+export default WaitingRoomPage;
