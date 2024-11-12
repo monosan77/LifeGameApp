cfeb606304cd5611cbc1e555c3dc088f3256fa4a
前田浩貴 <hiroki-m08@users.noreply.github.com>
2024-11-12 14:10:35 +0900

    test:video.tsx

--- src/components/TopPage/Video/video.test.tsx
+++ src/components/TopPage/Video/video.test.tsx
@@ -3,12 +3,10 @@ import Video from './video';
 
 describe('ビデオコンポーネント', () => {
   test('ビデオが正しく表示されているか', () => {
-    const { container } = render(<Video />);//renderには多くのプロパティを持つオブジェクトを返す。その中のcontainerは最上位のDOMを表す。
+    const { container } = render(<Video />); //renderには多くのプロパティを持つオブジェクトを返す。その中のcontainerは最上位のDOMを表す。
     const videoElement = container.querySelector('video');
 
     if (videoElement) {
-
-      
       expect(videoElement).toBeInTheDocument(); // videoタグがレンダリングされている場合のテスト
       expect(videoElement).toHaveAttribute('autoPlay'); //toHaveAttributeでvideoタグに属性があるかどうかをテスト
       expect(videoElement).toHaveAttribute('loop');
@@ -16,14 +14,14 @@ describe('ビデオコンポーネント', () => {
     } else {
       // videoタグが存在しない場合のテスト
       const fallbackText = container.querySelector('p'); //「Your browser does not support the video tag」を取得
-      expect(fallbackText).toBeInTheDocument();  //それが描画されているかを検証
+      expect(fallbackText).toBeInTheDocument(); //それが描画されているかを検証
     }
   });
 
   test('スタイルが当たっているか', () => {
     const { container } = render(<Video />);
     const divElement = container.querySelector('div');
-    expect(divElement).toHaveClass('video');//toHaveClassは、DOM要素に特定のCSSクラスが含まれているかどうかを検証するためのマッチャー
+    expect(divElement).toHaveClass('video'); //toHaveClassは、DOM要素に特定のCSSクラスが含まれているかどうかを検証するためのマッチャー
   });
 
   test('sourceの属性が当たっているかどうか', () => {
